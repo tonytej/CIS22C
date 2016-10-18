2a344bada0b5a45546f3281652eaa204b568167a
Tony <antonytej@gmail.com>
2016-10-18 00:06:38 -0700

    Finished the palindrome program.

--- Palindromes/Palindrome.cpp
+++ Palindromes/Palindrome.cpp
@@ -1,7 +1,7 @@
 /*
 Antony Tejakusuma
 CIS 22C
-StackTest.cpp
+Palindrome.cpp
 */
 
 #include "Stack.cpp"
@@ -17,16 +17,60 @@ int main(){
         exit(-1);
     }
 
-    string line;
-    
+    string line; // variable to store each line of the input
+    Queue Q; // Queue to hold the modified version of the phrases that can easily be processed.
+    Queue Q1; // Queue to store the original phrases with the symbols and spaces.
+    Stack S; // Stack to reverse the phrase
+
     while(getline(fin, line)){
-        str.erase(0, str.find_first_not_of(' '));       //prefixing spaces
-str.erase(str.find_last_not_of(' ')+1);
-        cout << line << endl;
+    	Q1.enqueue(line); // enqueue the original phrases
+        line.erase(remove(line.begin(), line.end(), ' '), line.end()); // Modifications to the phrases in order 
+        line.erase(remove(line.begin(), line.end(), '.'), line.end()); // to simplify the proses of checking Palindromes.
+        line.erase(remove(line.begin(), line.end(), '?'), line.end()); // These characters is not a part of palindrome
+        line.erase(remove(line.begin(), line.end(), '!'), line.end()); // checking and should be removed from the phrases.
+        line.erase(remove(line.begin(), line.end(), ','), line.end()); // The only thing that is needed is the alphabets of
+        line.erase(remove(line.begin(), line.end(), '\''), line.end()); // the phrases.
+        for(int i = 0; line[i]; i++){
+  			line[i] = tolower(line[i]); // another modification. convert every alphabets to lowercase for ease of comparing.
+		}	
+        Q.enqueue(line); // enqueue modified phrases
     }
+
+	while(Q.get_length() != 0){ // only checking Q because length of Q and Q1 is the same
+    	string str = Q.get_front();
+    	string ori = Q1.get_front();
+	   	for(int i = 0; i < str.size(); i++){ // iterate through the modified phrase
+	   		string letter(1, str[i]); // convert individual char to string
+	   		S.push(letter); // push to stack
+	   	}
+
+	   	string rev; // variable to hold the reversed string
+	   	while(S.get_length() != 0){ 
+	   		rev += S.get_top(); //combine each reversed letter together
+	   		S.pop();
+	   	}
+
+	   	if (str == rev){ // checking for palindromes
+	   		ori += " (Palindrome)";
+	   	} else {
+	   		ori += " (Not a Palindrome)";
+	   	}
+
+	   	cout << ori << endl; // print original phrases with its palindrome status
+	   	Q.dequeue(); // Processed phrase dequeued.
+	   	Q1.dequeue(); // On to the next one.
+    }  
 }
 
 /*
 Results:
-
+A man, a plan, a canal, Panama. (Palindrome)
+Do geese see God? (Palindrome)
+Never odd. Even. (Not a Palindrome)
+Never odd or even. (Palindrome)
+Eye (Palindrome)
+Ear (Not a Palindrome)
+Able was I ere I saw Elba! (Palindrome)
+Was it Eliot's toilet I saw? (Palindrome)
+Race cars (Not a palindrome)
 */
