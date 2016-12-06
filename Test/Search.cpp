c325d85cbbb022093611669634e3fcf9b5bd63a6
Tony <antonytej@gmail.com>
2016-12-05 20:12:57 -0800

    Finished and tested a part of the search engine that enables a string to be used as a hash table index.

--- Test/Search.cpp
+++ Test/Search.cpp
@@ -12,28 +12,26 @@ class Search {
 	private:
 		Hash_Table ht;
 		Hash_Table_Search hts;
-		string useless[20] = { "an", "the" , "of", "to", "and", "but", "nor", "or", "some", "any",
+		string useless[20] = { "a", "the" , "of", "to", "and", "but", "nor", "or", "some", "any",
 							"very", "in", "on", "at", "before", "after", "into", "over", "through", "along"};
 		string files[15] = { "almightygosh.txt", "areweready.txt", "byebye.txt", "electrify.txt", "explotar.txt", "highandlow.txt", "painting.txt",
 							 "paperroute.txt", "porcelain.txt", "raging.txt", "sameoldblues.txt", "sendthemoff.txt", "shelter.txt", "surprise.txt", "tearingmeup.txt"};
 	public:
 		void search(){
-			cout << "WHY" << endl;
+			int count = 0;
 			for (int i = 0; i < 15; i++){
 				ifstream fin(files[i]);
 				if(fin.fail()){
 					cout << "Input failed to open" << endl;
 					exit(-1);
 				}
-
-				int count = 0;
+				
 				string line;
 				getline(fin, line);
 				getline(fin, line);
 				getline(fin, line);
 				getline(fin, line);
 
-				cout << "clear" << endl;
 				while(getline(fin, line)){
 					vector<string> vec;
 					istringstream iss(line);
@@ -43,31 +41,38 @@ class Search {
 			        	transform(word.begin(), word.end(), word.begin(), ::tolower);
 			        	vec.push_back(word);
 			    	}
-
 			    	for(int k = 0; k < vec.size(); k++){
 			    		for(int j = 0; j < 20; j++){
-			    			if(vec[i] == useless[j]){
+			    			if(vec[k] == useless[j]){
 			    				vec.erase(remove_if(vec.begin(), vec.end(), [&]( const string& str )
 			    				{
 			        			return std::find(begin(useless), end(useless), str ) != end(useless);
-			    				}), vec.end()); 
+			    				}), vec.end());
 			    			}
 			    		}
-			    		wordID w(vec[i], -1);
+			    		wordID w(vec[k], -1);
 			    		if (hts.find(w) == -1){
 			    			w.setID(count);
 			    			hts.insert(w);
 			    			count++;
 			    		}
 					}
-				}				
+				}			
 			}
 		}
 
 		void print_hts(){
-			hts.print();
+			hts.print(fout);
 		}
 };
+
+int main(){
+	Search s;
+	cout << 2 << endl;
+	s.search();
+	cout << 3 << endl;
+	s.print_hts();
+}
 
 
 /*1. Read in a word from a file
